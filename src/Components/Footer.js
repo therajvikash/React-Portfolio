c7db68a4fdcea868a5198d27b2091cfda2ba490a
Vikash choudhary <vchy63660@gmail.com>
2020-09-08 16:15:26 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,9 +18,7 @@ class Footer extends Component {
               {networks}
            </ul>
 
-           <ul>
-            <li>Modified with love by the therajvikash ♥️</li>
-           </ul>
+            <span role ="img">Modified with love by the therajvikash ♥️</span>
 
            <ul className="copyright">
               <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
